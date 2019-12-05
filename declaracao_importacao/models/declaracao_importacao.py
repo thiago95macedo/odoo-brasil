e7e6dcaeac704f1ef953c6d5912968ae74a4f68a
Mackilem <mack.vdl@gmail.com>
2019-12-05 18:37:52 -0300

    backup_0512

--- declaracao_importacao/models/declaracao_importacao.py
+++ declaracao_importacao/models/declaracao_importacao.py
@@ -10,36 +10,51 @@ class DeclaracaoImportacao(models.Model):
 
     name = fields.Char(
         string="Número da DI", )
-    date_registro = fields.Date(
-        string="Data Registro", )
-    sequence = fields.Integer(
-        string="Sequence")
-    tag_ids = fields.Many2many(
-        string="Marcadores",
-        comodel_name="declaracao.tags",
-        ondelete="set null",
-        help="Explain your field.",)
-    declaracao_line_ids = fields.One2many(
-        string="Adições",
-        comodel_name="declaracao.importacao.line",
+    date_register = fields.Date(string="Data de Registro")
+    date_picking = fields.Date(string="Data de Embarque", )
+    seller_id = fields.Many2one(
+        string="Fornecedor",
+        comodel_name="res.partner",
+        domain="[('supplier', '!=', False), ('is_company', '!=', False)]",
+        ondelete="restrict")
+    gross_weight_total = fields.Float(
+        string="Peso Bruto Total",)
+    net_weight_total = fields.Float(
+        string="Peso Liquido Total",)
+    freight_currency_id = fields.Many2one(
+        string="Moeda Frete",
+        comodel_name="res.currency",
+        domain="[('active', '!=', False)]",
+        ondelete="set null")
+    insurance_currency_id = fields.Many2one(
+        string="Moeda Seguro",
+        comodel_name="res.currency",
+        domain="[('active', '!=', False)]",
+        ondelete="set null")
+    freight_total = fields.Float(string="Valor do Frete", )
+    insurance_total = fields.Float(string="Valor do Seguro", )
+    amount_total = fields.Float(string="Valor Total", )
+
+    product_line_ids = fields.One2many(
+        string="Produtos",
+        comodel_name="declaracao.product.line",
         inverse_name="declaracao_id")
 
 
-class DeclaracaoImportacaoLines(models.Model):
-    _name = "declaracao.importacao.line"
+class DeclaracaoProductLines(models.Model):
+    _name = "declaracao.product.line"
 
     declaracao_id = fields.Many2one(
-        string="Declaração",
+        string="DI",
         comodel_name="declaracao.importacao")
     product_id = fields.Many2one(
         string="Produto",
         comodel_name="product.product",
         ondelete="restrict")
     quantity = fields.Float(string="Quantidade")
-
-
-class DeclaracaoTags(models.Model):
-    _name = "declaracao.tags"
-    _description = "Marcadores para DI"
-
-    name = fields.Char(string="Descrição", )
+    price_unit = fields.Float(string="Preço Unit.")
+    price_subtotal = fields.Float(string="Subtotal.")
+    currency_id = fields.Many2one(
+        string="Moeda",
+        comodel_name="res.currency",
+        domain="[('active', '!=', False)]")
